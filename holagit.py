8cc2345c3a8934afa70d3b713cf67261d3f9d0d1
Rey <reynaldoperez2003@gmail.com>
2025-10-31 13:10:24 -0500

    se agrego gitignore

--- holagit.py
+++ holagit.py
@@ -3,4 +3,4 @@ print("Hola muchacho como tan")
 a = int(input("Ingresa el valor de a: "))
 b = int(input("Ingresa el valor de b: "))
 res = a-b
-print(f"El resultado es: {res}")
+print(f"Tu mama pesa: {res}")
